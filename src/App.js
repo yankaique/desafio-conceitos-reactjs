05b2a478f13ada4fbbe35d202039968eae3a6a43
Yan <yankaiquecosta.yk@gmail.com>
2020-10-31 21:51:32 -0300

    Renderização de componentes, adição e remoção

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from "react";
-
+import React,{useState, useEffect} from "react";
+import api from './services/api';
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+  const [randomTecnology, setRandomTecnology] = useState('react-native');
+
+  useEffect(()=>{
+    api.get('repositories').then(response => {setRepositories(response.data)})
+  },[])
+
   async function handleAddRepository() {
-    // TODO
+
+    if(randomTecnology=='react'){
+      setRandomTecnology('node');
+    }else if(randomTecnology == 'node'){
+      setRandomTecnology('react-native');
+    }else{
+      setRandomTecnology('react');
+    }
+
+    const response = await api.post('repositories',{title:`Uma aplicação feita com ${randomTecnology}`,
+    url:"https://github.com/yankaique",
+    techs:randomTecnology})
+
+    const newData = response.data;
+
+    setRepositories([...repositories,newData]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    await api.delete(`repositories/${id}`);
+    
+    const findIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+    repositories.splice(findIndex,1);
+
+    setRepositories([...repositories]);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+      {repositories.map(response => 
+        (<li key={response.id}>{response.title}  
+          <button onClick={() => handleRemoveRepository(response.id)}>
+          Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
